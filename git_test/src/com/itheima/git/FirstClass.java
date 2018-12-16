51890be4b2d037e290c8a3cdf7a981ffeeedf3cc
zly <1123762330@qq.com>
2018-12-16 08:50:40 +0800

    修改2

--- git_test/src/com/itheima/git/FirstClass.java
+++ git_test/src/com/itheima/git/FirstClass.java
@@ -5,4 +5,8 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("第二次修改");
     }
+
+    public void test(){
+        System.out.println("测试");
+    }
 }
